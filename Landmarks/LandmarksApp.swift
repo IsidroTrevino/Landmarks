19567c8419bf65d74e857ca67bb95c3908e3abc0
IsidroTrevino <A00837104@tec.mx>
2024-09-09 21:08:01 -0600

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Isidro Treviño on 09/09/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
